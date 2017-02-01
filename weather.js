3000ea3d72c3cb34945bbe8a49fb20199e9aaae1
amran16 <maryamg99@gmail.com>
2017-01-31 20:06:45 -0800

    update stylesheet

---
+++ weather.js
@@ -0,0 +1,52 @@
+var Api_Key = '0ecd65091ca9c34ed5953e2be578f1ff'; 
+var cel = false;
+
+$(document).ready(function(){
+  
+  //IP Geolocation API: http://ip-api.com/
+  
+  var Geo_Ip = 'http://ip-api.com/json';
+  
+  $.getJSON(Geo_Ip, function(data){
+    
+    var city = data.city;
+
+    var api = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=' + Api_Key;
+    
+   $.getJSON(api, function(wea_data){
+      
+      show(wea_data, cel);
+     
+      $('#fTemp').click(function(){
+          cel = !cel;
+          show(wea_data, cel);
+      });
+    });
+  });
+});
+
+function displayTemp(fTemp, c){
+  if(c) return Math.round((fTemp - 32) *(5/9)) + '&degC';
+  return Math.round(fTemp) + '&degF';
+}
+
+
+function show(data, cel){
+         var city = data.name;
+         var country = data.sys.country;
+         var icon = data.weather[0].icon;
+         var fTemp = displayTemp(data.main.temp, cel);
+         var fTemp_low = displayTemp(data.main.temp_min, cel);
+         var fTemp_high = displayTemp(data.main.temp_max, cel);
+         var weathertype = data.weather[0].description;
+         var windSpeed = data.wind.speed.toFixed(0);    
+         
+         $('#city').html(city + ', ' + country);
+         $('#fTemp').html(fTemp);
+         $('#high-low').html(fTemp_high + ' / ' + fTemp_low);
+         $('#windSpeed').html(windSpeed + ' mph');
+  
+         var iconSrc = "http://openweathermap.org/img/w/" + icon + ".png";
+
+         $('#icon').html('<img src="' + iconSrc + '">  ' + weathertype);
+}
